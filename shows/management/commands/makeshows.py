b03f9bb1c312bc758c3ef6a504d4217ffed5b627
gwhigs <gwhigs@gmail.com>
2014-11-01 18:19:49 -0400

    Updates for makeshows command

--- shows/management/commands/makeshows.py
+++ shows/management/commands/makeshows.py
@@ -2,16 +2,21 @@ from __future__ import unicode_literals
 
 import csv
 import re
+import os.path
 
 from django.core.management.base import BaseCommand
 from shows.models import Show
 
+SHOW_CSV_HEADERS = ['platform', 'showname', 'api_id']
+
 
 class Command(BaseCommand):
-    help = ('Makes new shows from a CSV list. '
-            'Default looks for makeshows.csv in projectroot/.')
+    help = ('Tries to create new shows from a CSV list. By default, '
+            'looks for makeshows.csv in projectroot/, but will take '
+            'another file path as an argument. If no file is found, '
+            'creates a sample template.')
 
-    def make_show(self, csvrow):
+    def make_show(self, csv_dict_row):
         """
         Creates a new Show object from a dict of show info.
         """
@@ -26,7 +31,16 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         fn = options['filename']
-        with open(fn, newline='') as f:
-            reader = csv.DictReader(f)
-            for row in reader:
-                self.make_show(row)
+        if os.path.isfile(fn):
+            # If the file exists, make the shows
+            with open(fn, newline='') as f:
+                reader = csv.DictReader(f)
+                for row in reader:
+                    self.make_show(row)
+        else:
+            # If the file doesn't exist, make a template and do nothing
+            with open(fn, 'w', newline='') as f:
+                writer = csv.writer(f)
+                writer.writerow(SHOW_CSV_HEADERS)
+                msg = 'No file found. See template created at {}'.format(fn)
+                self.stdout.write(msg)
